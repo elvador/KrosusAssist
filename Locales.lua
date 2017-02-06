c234f45d571ec2a7643ffe3bf900e780d2a37056
bullsei <bullsei@users.noreply.github.com>
2017-02-06 17:58:16 +0100

    Update Locales.lua
    German Localization

--- Locales.lua
+++ Locales.lua
@@ -4,26 +4,26 @@ local L
 
 L = LibStub("AceLocale-3.0"):NewLocale("Krosus Assist", "deDE")
 if L then
-	--L["Krosus Assist"] = true
-	--L["Disabled"] = true
-	--L["Disable the plugin."] = true
-	--L["Lock"] = true
-	--L["Locks the display in place, preventing moving and resizing."] = true
-	--L["Font"] = true
-	--L["Font size"] = true
-	--L["X Position"] = true
-	--L["Y Position"] = true
-	--L["Exact Positioning"] = true
-	--L["Type in the box or move the slider if you need exact positioning from the anchor."] = true
-	--L["Close"] = true
-	--L["Closes the display.\n\nTo disable it completely, you have to go into the options and toggle the 'disabled' checkbox."] = true
-	--L["The display will show next time. To disable it completely, you have to go into the options and toggle the 'disabled' checkbox."] = true
-	--L["Where was the first Fel Beam?"] = true
-	--L["left"] = true
-	--L["right"] = true
-	--L["You are using no or an outdated version of a boss mod. Please update your existing mod or download BigWigs."] = true
-	--L["Reset"] = true
-	--L["Resets the display to its default position"] = true
+	L["Krosus Assist"] = true
+	L["Disabled"] = "Deaktiviert"
+	L["Disable the plugin."] = "Deaktiviert das Plugin."
+	L["Lock"] = "Fixieren"
+	L["Locks the display in place, preventing moving and resizing."] = "Verankert die Position der Anzeige, dies verhindert Bewegung und Größenänderung."
+	L["Font"] = "Schriftart"
+	L["Font size"] = "Schriftgröße"
+	L["X Position"] = "X-Position"
+	L["Y Position"] = "Y-Position"
+	L["Exact Positioning"] = "Exakte Positionierung"
+	L["Type in the box or move the slider if you need exact positioning from the anchor."] = "Tippe in das Eingabefeld oder bewege den Schieber, wenn du den Anker genau positionieren möchtest."
+	L["Close"] = "Schließen"
+	L["Closes the display.\n\nTo disable it completely, you have to go into the options and toggle the 'disabled' checkbox."] = "Schließt die Anzeige. Um sie komplett zu deaktivieren, musst du in den Einstellungen die Option Deaktiviert anhaken."
+	L["The display will show next time. To disable it completely, you have to go into the options and toggle the 'disabled' checkbox."] = "Die Anzeige wird das nächste Mal erscheinen. Um sie komplett zu deaktivieren, musst du in den Einstellungen die Option Deaktiviert anhaken."
+	L["Where was the first Fel Beam?"] = "Wo war der erste Teufelsstrahl?"
+	L["left"] = "links"
+	L["right"] = "rechts"
+	L["You are using no or an outdated version of a boss mod. Please update your existing mod or download BigWigs."] = "Du verwendest keine oder eine veraltete Version eines Bossmods. Bitte aktualisiere dein vorhandenes Bossmod-Addon oder lade BigWigs herunter."
+	L["Reset"] = "Zurücksetzen"
+	L["Resets the display to its default position"] = "Setzt die Anzeige auf seine Standardposition zurück."
 end
 
 L = LibStub("AceLocale-3.0"):NewLocale("Krosus Assist", "esES")

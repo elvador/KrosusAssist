d191d488bd75836e55d963be01140396d61c2e03
elvador <reysptools@googlemail.com>
2017-02-09 17:37:32 +0100

    Add zhTW localization (thanks to BNSSNB!)

--- Locales.lua
+++ Locales.lua
@@ -220,24 +220,24 @@ end
 
 L = LibStub("AceLocale-3.0"):NewLocale("Krosus Assist", "zhTW")
 if L then
-	--L["Krosus Assist"] = true
-	--L["Disabled"] = true
-	--L["Disable the plugin."] = true
-	--L["Lock"] = true
-	--L["Locks the display in place, preventing moving and resizing."] = true
-	--L["Font"] = true
-	--L["Font size"] = true
-	--L["X Position"] = true
-	--L["Y Position"] = true
-	--L["Exact Positioning"] = true
-	--L["Type in the box or move the slider if you need exact positioning from the anchor."] = true
-	--L["Close"] = true
-	--L["Closes the display.\n\nTo disable it completely, you have to go into the options and toggle the 'disabled' checkbox."] = true
-	--L["The display will show next time. To disable it completely, you have to go into the options and toggle the 'disabled' checkbox."] = true
-	--L["Where was the first Fel Beam?"] = true
-	--L["left"] = true
-	--L["right"] = true
-	--L["You are using no or an outdated version of a boss mod. Please update your existing mod or download BigWigs."] = true
-	--L["Reset"] = true
-	--L["Resets the display to its default position"] = true
+	L["Krosus Assist"] = "克羅索斯助手"
+	L["Disabled"] = "停用"
+	L["Disable the plugin."] = "停用此插件。"
+	L["Lock"] = "鎖定"
+	L["Locks the display in place, preventing moving and resizing."] = "鎖定視窗在位置上，防止移動或改變大小。"
+	L["Font"] = "字體"
+	L["Font size"] = "字體大小"
+	L["X Position"] = "水平位置"
+	L["Y Position"] = "垂直位置"
+	L["Exact Positioning"] = "精確定位"
+	L["Type in the box or move the slider if you need exact positioning from the anchor."] = "在輸入框鍵入數字或移動滑桿如果你有需要精確的定位。"
+	L["Close"] = "關閉"
+	L["Closes the display.\n\nTo disable it completely, you have to go into the options and toggle the 'disabled' checkbox."] = "關閉視窗。\n\n要完全停用，你應該要到選項中勾選'停用'的選項。"
+	L["The display will show next time. To disable it completely, you have to go into the options and toggle the 'disabled' checkbox."] = "視窗將會在下次顯示。要完全停用，你應該要到選項中勾選'停用'的選項。"
+	L["Where was the first Fel Beam?"] = "第一個魔化能量光束在哪邊？"
+	L["left"] = "左"
+	L["right"] = "右"
+	L["You are using no or an outdated version of a boss mod. Please update your existing mod or download BigWigs."] = "你未使用或是使用的首領提醒插件已過期。請更新你正使用的首領提醒插件或是下載BigWigs。"
+	L["Reset"] = "重置"
+	L["Resets the display to its default position"] = "重置視窗到預設位置"
 end
